ffbfb7302e3a2ed3e92047f6deec9240cbef8691
Dwarakamai <Dwarakamai@DESKTOP-C9V8J8K>
2017-11-08 17:04:48 -0600

    change 3

--- E2ESeleniumJenkins/src/testcases/TC_001.java
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -12,7 +12,7 @@ public class TC_001 {
 		//tc1
 		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://gmail.google.com");
+		driver.get("https://www.google.com/");
 		driver.quit();
 	}
 	
